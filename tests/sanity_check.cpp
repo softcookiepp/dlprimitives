207c2b4ccc822e2fa04fc743a8970ce608ebf263
softcookiepp <softcookiepp@mailfence.com>
2025-12-31 16:19:00 -0800

    WHYYYY

---
+++ tests/sanity_check.cpp
@@ -0,0 +1,32 @@
+#include <dlprim/random.hpp>
+#include <dlprim/tensor.hpp>
+#include <dlprim/ops/initialization.hpp>
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+#include <limits>
+#include "test.hpp"
+
+namespace dp = dlprim;
+
+int main(int argc, char** argv)
+{
+	dp::Context ctx(argv[1]);
+	dp::ExecutionContext e=ctx.make_execution_context();
+	
+	dp::Tensor t(ctx, dp::Shape(10) );
+	std::vector<float> values(10);
+	for (size_t i = 0; i < 10; i += 1)
+		values[i] = (float)i;
+	
+	t.to_device(e, values.data());
+	std::vector<float> expected(10, 0.0);
+	t.to_host(e, expected.data());
+	for (size_t i = 0; i < 10; i += 1)
+	{
+		std::cout << ((expected[i] == values[i]) ? "equal!" : "not!") << std::endl;
+	}
+	
+	return 0;
+	
+}
