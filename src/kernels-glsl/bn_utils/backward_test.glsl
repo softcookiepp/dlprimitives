a8826e9374a86f0cb8065926067645bbec230d47
softcookiepp <softcookiepp@mailfence.com>
2026-01-09 14:31:44 -0800

    backward_test reimplemented

--- src/kernels-glsl/bn_utils/backward_test.glsl
+++ src/kernels-glsl/bn_utils/backward_test.glsl
@@ -1,23 +1,44 @@
 #version 450
-__kernel
-void backward_test(int batches,int channels,int HW,
-             __global float *dx,ulong  dx_offset,
-             __global float const *dy,ulong  dy_offset,
-             __global float const *a,ulong  a_offset,
-             float factor)
+
+#include "../common/defs.glsl"
+#include "bn_defs.glsl"
+#include "../common/workgroup.glsl"
+
+#if USE_BDA == 0
+	layout(binding = 0, std430) buffer dx_buf { float dx[]; };
+	layout(binding = 1, std430) readonly buffer dy_buf { float dy[]; };
+	layout(binding = 2, std430) readonly buffer a_buf { float a[]; };
+#endif
+
+layout(push_constant, std430) uniform backward_test
 {
-    int b  = get_global_id(DIM_B);
-    int f  = get_global_id(DIM_F);
-    int rc = get_global_id(DIM_RC);
+	uint batches; uint channels; uint HW;
+#if USE_BDA
+	__global float *dx;
+#endif
+	uint dx_offset;
+#if USE_BDA
+	__global float const *dy;
+#endif
+	uint dy_offset;
+#if USE_BDA
+	__global float const *a;
+#endif
+	uint a_offset;
+	float factor;
+};
+        
+        void main()     
+{
+    uint b  = get_global_id(DIM_B);
+    uint f  = get_global_id(DIM_F);
+    uint rc = get_global_id(DIM_RC);
     if(b >= batches || f >= channels || rc >= HW)
         return;
-    dx+=dx_offset;
-    dy+=dy_offset;
-    a+=a_offset;
-    int pos = (b * channels + f) * HW + rc;
-    float val = dy[pos] * a[f];
+    uint pos = (b * channels + f) * HW + rc;
+    float val = dy[pos + dy_offset] * a[f + a_offset];
     if(factor == 0)
-        dx[pos] = val;
+        dx[pos + dx_offset] = val;
     else
-        dx[pos] = dx[pos]*factor + val;
+        dx[pos + dx_offset] = dx[pos + dx_offset]*factor + val;
 }
