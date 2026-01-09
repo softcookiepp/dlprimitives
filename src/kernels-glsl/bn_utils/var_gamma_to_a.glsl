2ab6f4bf68c5ab209f1cfe3a9c8c63004ce31100
softcookiepp <softcookiepp@mailfence.com>
2026-01-09 13:37:27 -0800

    reimplemented var_gamma_to_a

--- src/kernels-glsl/bn_utils/var_gamma_to_a.glsl
+++ src/kernels-glsl/bn_utils/var_gamma_to_a.glsl
@@ -1,21 +1,41 @@
 #version 450
+#include "../common/defs.glsl"
+#include "bn_defs.glsl"
+#include "../common/workgroup.glsl"
 
-__kernel
-void var_gamma_to_a(int N,float eps,
-              __global float const * var, ulong  var_offset,
-              __global float const * gamma, ulong  gamma_offset,
-              __global float *a,ulong  a_offset)
+#if USE_BDA == 0
+	layout(binding = 0, std430) readonly buffer var_buf { float var[]; };
+	layout(binding = 1, std430) readonly buffer gamma_buf { float gamma[]; };
+	layout(binding = 2, std430) buffer a_buf { float a[]; };
+#endif
+
+layout(push_constant, std430) uniform var_gamma_to_a
 {
-    int pos = get_global_id(0);
+	int N;float eps;
+#if USE_BDA
+	__global float const * var;
+#endif
+	uint var_offset;
+#if USE_BDA
+	__global float const * gamma;
+#endif
+	uint gamma_offset;
+	bool use_gamma; // added since buffers can't be evaluated as boolean statements
+#if USE_BDA
+	__global float *a;
+#endif
+	uint a_offset;
+};
+
+void main()
+{
+    uint pos = get_global_id(0);
     if(pos >= N)
         return;
-    var  += var_offset;
-    gamma += gamma_offset;
-    a += a_offset;
-    float scale = 1.0f / sqrt(var[pos] + eps);
-    if(gamma)
-        scale *= gamma[pos];
-    a[pos] = scale;
+    float scale = 1.0f / sqrt(var[pos + var_offset] + eps);
+    if(use_gamma)
+        scale *= gamma[pos + gamma_offset];
+    a[pos + a_offset] = scale;
 }
 
 
