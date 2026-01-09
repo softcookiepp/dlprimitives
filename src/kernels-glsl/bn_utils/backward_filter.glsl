6226b6a76393bfc695c75db76e951e0c91e20bc9
softcookiepp <softcookiepp@mailfence.com>
2026-01-09 14:17:04 -0800

    backward_filter reimplemented

--- src/kernels-glsl/bn_utils/backward_filter.glsl
+++ src/kernels-glsl/bn_utils/backward_filter.glsl
@@ -1,41 +1,71 @@
 #version 450
+#include "../common/defs.glsl"
+#include "bn_defs.glsl"
+#include "../common/workgroup.glsl"
 
-__kernel
-void backward_filter(int N,
-                    __global float const *mean,ulong  mean_offset,
-                    __global float const *var, ulong  var_offset,
-                    __global float const *dy_sum, ulong  dy_sum_offset,
-                    __global float const *dyx_sum,ulong  dyx_sum_offset,
-                    __global float *dgamma,ulong  dgamma_offset,
-                    __global float *dbeta,ulong  dbeta_offset,
-                    float eps,
-                    float factor_gamma,
-                    float factor_beta)
+#if USE_BDA == 0
+	layout(binding = 0, std430) readonly buffer mean_buf { float mean[]; };
+	layout(binding = 1, std430) readonly buffer var_buf { float var[]; };
+	layout(binding = 2, std430) readonly buffer dy_sum_buf { float dy_sum[]; };
+	layout(binding = 3, std430) readonly buffer dyx_sum_buf { float dyx_sum[]; };
+	layout(binding = 4, std430) buffer dgamma_buf { float dgamma[]; };
+	layout(binding = 5, std430) buffer dbeta_buf { float dbeta[]; };
+#endif
+
+layout(push_constant, std430) uniform backward_filter
 {
-    int i=get_global_id(0);
+	int N;
+#if USE_BDA
+	__global float const *mean;
+#endif
+	uint  mean_offset;
+#if USE_BDA
+	__global float const *var;
+#endif
+	uint  var_offset;
+#if USE_BDA
+	__global float const *dy_sum;
+#endif
+	uint  dy_sum_offset;
+#if USE_BDA
+	__global float const *dyx_sum;
+#endif
+	uint  dyx_sum_offset;
+#if USE_BDA
+	__global float *dgamma;
+#endif
+	uint dgamma_offset;
+	bool use_gamma; // here we are again
+#if USE_BDA
+	__global float *dbeta;
+#endif
+	uint dbeta_offset;
+	bool use_beta;
+	float eps;
+	float factor_gamma;
+	float factor_beta;
+};
+
+void main()
+{
+    uint i=get_global_id(0);
     if(i >= N)
         return;
-    mean += mean_offset;
-    var  += var_offset;
-    dy_sum += dy_sum_offset;
-    dyx_sum += dyx_sum_offset;
 
-    float dys = dy_sum[i];
+    float dys = dy_sum[i + dy_sum_offset];
 
-    if(dgamma) {
-        dgamma += dgamma_offset;
-        float dG = (dyx_sum[i] - mean[i]*dys) / sqrt(var[i] + eps); 
+    if(use_gamma) {
+        float dG = (dyx_sum[i + dyx_sum_offset] - mean[i + mean_offset]*dys) / sqrt(var[i + var_offset] + eps); 
         if(factor_gamma == 0)
-            dgamma[i] = dG;
+            dgamma[i + dgamma_offset] = dG;
         else
-            dgamma[i] = dgamma[i]*factor_gamma + dG;
+            dgamma[i + dgamma_offset] = dgamma[i + dgamma_offset]*factor_gamma + dG;
     }
     
-    if(dbeta) {
-        dbeta += dbeta_offset;
+    if(use_beta) {
         if(factor_beta == 0)
-            dbeta[i] = dys;
+            dbeta[i + dbeta_offset] = dys;
         else
-            dbeta[i] = dbeta[i] * factor_beta + dys;
+            dbeta[i + dbeta_offset] = dbeta[i + dbeta_offset] * factor_beta + dys;
     }
 }
