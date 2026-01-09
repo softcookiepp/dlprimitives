5cc9e31f419362ffa13524fa2bd49a631b16a04b
softcookiepp <softcookiepp@mailfence.com>
2026-01-09 13:09:36 -0800

    mean_var_to_a_b ported

--- src/kernels-glsl/bn_utils/mean_var_to_a_b.glsl
+++ src/kernels-glsl/bn_utils/mean_var_to_a_b.glsl
@@ -1,11 +1,36 @@
 #version 450
+#include "../common/defs.glsl"
+#include "bn_defs.glsl"
+#include "../common/workgroup.glsl"
 
+#if USE_BDA == 0
+	layout(binding = 0, std430) readonly buffer mean_buf { float mean[]; };
+	layout(binding = 1, std430) readonly buffer var_buf { float var[]; };
+	layout(binding = 2, std430) buffer a_buf { float a[]; };
+	layout(binding = 3, std430) buffer b_buf { float b[]; };
+#endif
 
-void mean_var_to_a_b(int N,float eps,
-                     __global float const * mean,uint  mean_offset,
-                     __global float const * var, uint  var_offset,
-                     __global float *a,uint  a_offset,
-                     __global float *b,uint  b_offset)
+layout(push_constant, std430) uniform mean_var_to_a_b
+{
+	int N;float eps;
+#if USE_BDA
+	__global float const * mean;
+#endif
+	uint mean_offset;
+#if USE_BDA
+	__global float const * var;
+#endif
+	uint var_offset;
+#if USE_BDA
+	__global float *a;
+#endif
+	uint a_offset;
+#if USE_BDA
+	__global float *b;
+#endif
+	uint b_offset;
+};
+
 void main()
 {
     uint pos = get_global_id(0);
