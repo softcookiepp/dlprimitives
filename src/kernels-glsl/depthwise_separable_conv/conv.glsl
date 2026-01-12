67677176ad9bb684fe2b41d5feae4a16bdd28fd7
softcookiepp <softcookiepp@mailfence.com>
2026-01-12 15:29:33 -0800

    implemented conv

--- src/kernels-glsl/depthwise_separable_conv/conv.glsl
+++ src/kernels-glsl/depthwise_separable_conv/conv.glsl
@@ -1,3 +1,4 @@
+#version 450
 ///////////////////////////////////////////////////////////////////////////////
 ///
 /// Copyright (c) 2021-2022 Artyom Beilis <artyomtnk@yahoo.com>
@@ -6,7 +7,8 @@
 ///
 ///////////////////////////////////////////////////////////////////////////////
 #include "../common/defs.glsl"
-#include "../common/atomic.glsl"
+#include "defs.glsl"
+//#include "../common/atomic.glsl"
 
 #define DIM_R 0
 #define DIM_C 1
@@ -17,39 +19,68 @@
 #define PATCH_H (PATCH_ROWS + KERN - 1)
 #define PATCH_W (PATCH_COLS + KERN - 1)
 
-void conv(int batch,int height,int width,
-          __global const float *input,ulong input_offset,
-          __global const float *kern,ulong kernel_offset,
-#if BIAS != 0
-          __global const float *bias,ulong bias_offset,
-#endif          
-          __global float *output,ulong output_offset,
-          float scale)
+#if USE_BDA == 0
+	layout(binding = 0, std430) readonly buffer inp_buf { float inp[]; };
+	layout(binding = 1, std430) readonly buffer kern_buf { float kern[]; };
+	layout(binding = 2, std430) readonly buffer bias_buf { float bias[]; };
+	layout(binding = 3, std430) buffer outp_buf { float outp[]; };
+#endif
+
+layout(push_constant, std430) uniform conv
 {
-    input += input_offset;
-    output += output_offset;
-    kern += kernel_offset;
-    int r = get_global_id(DIM_R) * PATCH_ROWS;
-    int c = get_global_id(DIM_C) * PATCH_COLS;
-    int b = get_global_id(DIM_BD) / CHANNELS;
-    int d = get_global_id(DIM_BD) % CHANNELS;
+	uint batch;
+	uint height;
+	uint width;
+#if USE_BDA
+	__global const float *input;
+#endif
+	uint input_offset;
+#if USE_BDA
+	__global const float *kern;
+#endif
+	uint kernel_offset;
+#if BIAS != 0
+#if USE_BDA
+	__global const float *bias;
+#endif
+	uint bias_offset;
+#endif          
+#if USE_BDA
+	__global float *output;
+#endif
+	uint output_offset;
+	float scale;
+};
+          
+void main()
+{
+    uint inp_offset = input_offset;
+    uint outp_offset = output_offset;
+    uint kern_offset = kernel_offset;
+    uint r = get_global_id(DIM_R) * PATCH_ROWS;
+    uint c = get_global_id(DIM_C) * PATCH_COLS;
+    uint b = get_global_id(DIM_BD) / CHANNELS;
+    uint d = get_global_id(DIM_BD) % CHANNELS;
 
     if(r >= height || c >= width || b >= batch)
         return;
 
-    kern += d * KERN * KERN;
+    kern_offset += (d * KERN * KERN);
     
-    input  += b * CHANNELS * width * height + d * width * height + (r - KERN_PAD) * width + c - KERN_PAD;
-    output += b * CHANNELS * width * height + d * width * height + r * width + c;
+    inp_offset  += (b * CHANNELS * width * height + d * width * height + (r - KERN_PAD) * width + c - KERN_PAD);
+    outp_offset += (b * CHANNELS * width * height + d * width * height + r * width + c);
 
     float K_vals[KERN][KERN];
     float I_vals[PATCH_H][PATCH_W];
 
-    #pragma unroll
-    for(int dr=0;dr < KERN;dr++)
-        #pragma unroll
-        for(int dc=0;dc<KERN;dc++)
-            K_vals[dr][dc] = *kern ++;
+    // #pragma unroll
+    for(uint dr=0;dr < KERN;dr++)
+        // #pragma unroll
+        for(uint dc=0;dc<KERN;dc++)
+        {
+            K_vals[dr][dc] = kern[kern_offset];
+            kern_offset += 1;
+		}
 
     #if BIAS != 0
         float start_val = bias[bias_offset + d];
@@ -59,45 +90,45 @@ void conv(int batch,int height,int width,
 
             
 
-    int y = r-KERN_PAD;
-    #pragma unroll
-    for(int dr=0;dr<PATCH_H;dr++,y++) {
+    uint y = r-KERN_PAD;
+    // #pragma unroll
+    for(uint dr=0;dr<PATCH_H;dr++,y++) {
         if(y < 0 || y >= height) {
-            #pragma unroll
-            for(int dc=0;dc<PATCH_W;dc++)
+            // #pragma unroll
+            for(uint dc=0;dc<PATCH_W;dc++)
                 I_vals[dr][dc]=0;
         }
         else {
-            int x = c - KERN_PAD;
-            #pragma unroll
-            for(int dc=0;dc<PATCH_W;dc++,x++) {
-                I_vals[dr][dc]=(0 <= x && x < width) ? input[dr*width+dc] : 0;
+            uint x = c - KERN_PAD;
+            // #pragma unroll
+            for(uint dc=0;dc<PATCH_W;dc++,x++) {
+                I_vals[dr][dc]=(0 <= x && x < width) ? inp[dr*width+dc + inp_offset] : 0;
             }
         }
     }
 
 
-    #pragma unroll
-    for(int dr=0;dr<PATCH_ROWS;dr++) {
+    // #pragma unroll
+    for(uint dr=0;dr<PATCH_ROWS;dr++) {
         if(r+dr >= height)
             break;
-        #pragma unroll
-        for(int dc=0;dc<PATCH_COLS;dc++) {
+        // #pragma unroll
+        for(uint dc=0;dc<PATCH_COLS;dc++) {
             if(c+dc>=width)
                 break;
             float sum = start_val;
-            #pragma unroll
-            for(int drk=0;drk < KERN;drk++)
-                #pragma unroll
-                for(int dck=0;dck<KERN;dck++)
-                    sum = mad(K_vals[drk][dck],I_vals[dr+drk][dc+dck],sum);
+            // #pragma unroll
+            for(uint drk=0;drk < KERN;drk++)
+                // #pragma unroll
+                for(uint dck=0;dck<KERN;dck++)
+                    sum = fma(K_vals[drk][dck],I_vals[dr+drk][dc+dck],sum);
 
             float value = ACTIVATION_F(sum);
-            __global float *optr = output + dr*width+dc;
+            uint optr = outp_offset + dr*width+dc;
             if(scale == 0.0)
-                *optr = value;
+                outp[optr] = value;
             else
-                *optr = scale * *optr + value;
+                outp[optr] = scale * outp[optr] + value;
         }
     }
 
