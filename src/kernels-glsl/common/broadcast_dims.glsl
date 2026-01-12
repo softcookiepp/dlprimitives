f8a4cfff2b18f6e4d91787ab034b4eb82304f046
softcookiepp <softcookiepp@mailfence.com>
2026-01-12 12:46:14 -0800

    copy_strided seems to build

--- src/kernels-glsl/common/broadcast_dims.glsl
+++ src/kernels-glsl/common/broadcast_dims.glsl
@@ -6,7 +6,7 @@
 ///
 ///////////////////////////////////////////////////////////////////////////////
 typedef struct Shape {
-    ulong s[DIMS];
+    uint s[DIMS];
 } Shape;
 
 
@@ -46,7 +46,7 @@ inline Shape get_pos_broadcast(Shape limits)
     r.s[2] = get_global_id(1) / limits.s[3];      
     r.s[3] = get_global_id(1) % limits.s[3];      
     r.s[4] = get_global_id(0) / (limits.s[5]*limits.s[6]);      
-    ulong s56 = get_global_id(0) % (limits.s[5]*limits.s[6]);
+    uint s56 = get_global_id(0) % (limits.s[5]*limits.s[6]);
     r.s[5] = s56 / limits.s[6];      
     r.s[6] = s56 % limits.s[6];      
 #elif DIMS == 8
@@ -54,12 +54,12 @@ inline Shape get_pos_broadcast(Shape limits)
     r.s[1] = get_global_id(2) % limits.s[1];      
 
     r.s[2] = get_global_id(1) / (limits.s[3]*limits.s[4]);      
-    ulong s34 = get_global_id(1) % (limits.s[3]*limits.s[4]);
+    uint s34 = get_global_id(1) % (limits.s[3]*limits.s[4]);
     r.s[3] = s34 / limits.s[4];      
     r.s[4] = s34 % limits.s[4];      
 
     r.s[5] = get_global_id(0) / (limits.s[6]*limits.s[7]);      
-    ulong s67 = get_global_id(0) % (limits.s[6]*limits.s[7]);
+    uint s67 = get_global_id(0) % (limits.s[6]*limits.s[7]);
     r.s[6] = s67 / limits.s[7];      
     r.s[7] = s67 % limits.s[7];      
 #else
---
+++ src/kernels-glsl/copy.glsl
@@ -0,0 +1,46 @@
+#version 450
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Copyright (c) 2021-2022 Artyom Beilis <artyomtnk@yahoo.com>
+///
+/// MIT License, see LICENSE.TXT
+///
+///////////////////////////////////////////////////////////////////////////////
+#include "common/defs.glsl"
+
+#if USE_BDA == 0
+	layout(binding = 0, std430) buffer target_buf { dtype target[]; };
+	layout(binding = 1, std430) buffer source_buf { dtype source[]; };
+#endif
+
+layout(push_constant, std430) uniform copy
+{
+	uint slice;
+	uint dim0;
+	uint dim1_tgt;uint dim1_tgt_offset;
+	uint dim1_src;uint dim1_src_offset;
+	uint dim2;
+#if USE_BDA
+	__global dtype *target;
+#endif
+	uint target_offset;
+#if USE_BDA
+	__global dtype const *source;
+#endif
+	uint source_offset;
+	dtype scale;
+};
+void main()
+{
+    uint p0 = get_global_id(0);
+    uint p1 = get_global_id(1);
+    uint p2 = get_global_id(2);
+    if(p0 >= dim2 || p1 >= slice || p2 >= dim0)
+        return;
+	uint _target_offset = target_offset + (p0 + (p1 + dim1_tgt_offset) * dim2 + p2 * (dim1_tgt * dim2));
+    uint _source_offset = source_offset + (p0 + (p1 + dim1_src_offset) * dim2 + p2 * (dim1_src * dim2));
+    if(scale == 0.0)
+        target[_target_offset] = source[_source_offset];
+    else
+        target[_target_offset] = scale * target[_target_offset] + source[_source_offset];
+}
