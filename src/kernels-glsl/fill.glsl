16861d48e6cdc45b76a73e957431c457df51f370
softcookiepp <softcookiepp@protonmail.com>
2026-01-18 13:37:46 -0800

    translated fill.glsl

---
+++ src/kernels-glsl/fill.glsl
@@ -0,0 +1,32 @@
+#version 450
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Copyright (c) 2021-2022 Artyom Beilis <artyomtnk@yahoo.com>
+///
+/// MIT License, see LICENSE.TXT
+///
+///////////////////////////////////////////////////////////////////////////////
+#include "common/defs.glsl"
+#include "common/workgroup.glsl"
+
+#if USE_BDA == 0
+	layout(binding = 0, std430) writeonly buffer p_buf { dtype p[]; };
+#endif
+
+layout(push_constant, std430) uniform fill
+{
+	uint total;
+#if USE_BDA
+	__global dtype *p;
+#endif
+	uint p_offset;
+	dtype value;
+};
+
+void main()	
+{
+    uint pos = get_global_id(0);
+    if(pos >= total)
+        return;
+    p[p_offset + pos ] = value;
+}
