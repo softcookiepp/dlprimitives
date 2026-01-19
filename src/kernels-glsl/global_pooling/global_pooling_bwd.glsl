35161b3c79e5ea1cc2a181a5cd13d4de5d4a6f54
softcookiepp <softcookiepp@mailfence.com>
2026-01-19 10:06:31 -0800

    implemented global_pooling_bwd.glsl

--- src/kernels-glsl/global_pooling/global_pooling_bwd.glsl
+++ src/kernels-glsl/global_pooling/global_pooling_bwd.glsl
@@ -10,45 +10,75 @@
 #define ITEMS_PER_WI 1
 #endif
 
-__kernel 
-__attribute__((reqd_work_group_size(1,WG_SIZE,1)))
-void global_pooling_bwd(int items,int over,float scale,
-                        #if POOL_MODE == 0
-                        __global const dtype *x, ulong  x_offset,
-                        #endif
-                        __global dtype *dx,ulong  dx_offset,__global const dtype *out,ulong  out_offset,
-                        dtype factor)
+layout(local_size_x = 1, local_size_y = WG_SIZE, local_size_z = 1) in;
+
+#if USE_BDA == 0
+	#if POOL_MODE == 0
+		layout(binding = 0, std430) readonly buffer x_buf { dtype x[]; };
+		layout(binding = 1, std430) buffer dx_buf { dtype dx[]; };
+		layout(binding = 2, std430) readonly buffer outp_buf { dtype outp[]; };
+	#else
+		layout(binding = 0, std430) buffer dx_buf { dtype dx[]; };
+		layout(binding = 1, std430) readonly buffer outp_buf { dtype outp[]; };
+	#endif
+#endif
+
+layout(push_constant, std430) uniform global_pooling_bwd
+{
+	uint items; uint over; float scale;
+	#if POOL_MODE == 0
+#if USE_BDA
+		__global const dtype *x; // readonly
+#endif
+		uint  x_offset;
+	#endif
+#if USE_BDA
+	__global dtype *dx; // read + write
+#endif
+	uint  dx_offset;
+#if USE_BDA
+	__global const dtype *outp; // readonly
+#endif
+	uint  out_offset;
+	dtype factor;
+};
+
+REDUCE_PREPARE(WG_SIZE, dtype);
+shared uint reduce_indx[WG_SIZE];
+shared dtype reduce_vals[WG_SIZE];
+        
+void main()
 {
     #if POOL_MODE == 0
-    x += x_offset;
+    uint x_ = x_offset;
     #endif
-    dx += dx_offset;
-    out += out_offset;
+    uint dx_ = dx_offset;
+    uint out_ = out_offset;
     
-    int b = get_global_id(0);
+    uint b = get_global_id(0);
 
     if(b >= items)
         return;
 
-    int c = get_global_id(1) * ITEMS_PER_WI;
+    uint c = get_global_id(1) * ITEMS_PER_WI;
 
     #if POOL_MODE == 0
-    x  += b * over;
+    x_  += b * over;
     #endif
-    dx += b * over;
-    out += b;
+    dx_ += b * over;
+    out_ += b;
 
 
     
-    REDUCE_PREPARE(WG_SIZE,dtype);
+    // REDUCE_PREPARE(WG_SIZE,dtype);
 
 #if POOL_MODE == 0
 
     dtype val = -DTYPE_MAX;
-    int index = -1;
-    for(int i=0;i<ITEMS_PER_WI;i++) {
+    uint index = -1;
+    for(uint i=0;i<ITEMS_PER_WI;i++) {
         if(c+i < over) {
-            dtype tmp = x[c+i];
+            dtype tmp = x[c+i + x_];
             if(tmp > val) {
                 index = c+i;
                 val = tmp;
@@ -56,21 +86,21 @@ void global_pooling_bwd(int items,int over,float scale,
         }
     }
 
-    __local int reduce_indx[WG_SIZE];
-    __local dtype reduce_vals[WG_SIZE];
+    // __local uint reduce_indx[WG_SIZE];
+    // __local dtype reduce_vals[WG_SIZE];
 
-    int lid = my_get_local_wg_id();
+    uint lid = my_get_local_wg_id();
     reduce_indx[lid] = index;
     reduce_vals[lid] = val;
-    barrier(CLK_LOCAL_MEM_FENCE); 
-    for(int i=WG_SIZE / 2; i > 0 ; i>>=1) {
+    barrier();
+    for(uint i=WG_SIZE / 2; i > 0 ; i>>=1) {
         if(lid < i) {
             dtype val;
-            int ind;
+            uint ind;
             dtype vl = reduce_vals[lid];
-            int   il = reduce_indx[lid];
+            uint   il = reduce_indx[lid];
             dtype vr = reduce_vals[lid+i];
-            int   ir = reduce_indx[lid+i];
+            uint   ir = reduce_indx[lid+i];
             if(vl > vr) {
                 ind=il;
                 val=vl;
@@ -92,29 +122,29 @@ void global_pooling_bwd(int items,int over,float scale,
             reduce_vals[lid] = val;
             reduce_indx[lid] = ind;
         }
-        barrier(CLK_LOCAL_MEM_FENCE); 
+        barrier(); 
     }
 
-    int target_index = reduce_indx[0];
-    dtype store = *out;
-    for(int i=0;i<ITEMS_PER_WI;i++) {
+    uint target_index = reduce_indx[0];
+    dtype store = outp[out_];
+    for(uint i=0;i<ITEMS_PER_WI;i++) {
         if(c+i < over) {
             dtype val = (c + i == target_index) ? store : 0;
             if(factor == 0)
-                dx[c+i] = val;
+                dx[c+i + dx_] = val;
             else
-                dx[c+i] = dx[c+i] * factor + val;
+                dx[c+i + dx_] = dx[c+i + dx_] * factor + val;
         }
     }
 #else
-    dtype store = *out * scale;
+    dtype store = outp[out_] * scale;
     
-    for(int i=0;i<ITEMS_PER_WI;i++) {
+    for(uint i=0;i<ITEMS_PER_WI;i++) {
         if(c+i < over) {
             if(factor == 0)
-                dx[c+i] = store;
+                dx[c+i + dx_] = store;
             else
-                dx[c+i] = dx[c+i] * factor + store;
+                dx[c+i + dx_] = dx[c+i + dx_] * factor + store;
         }
     }
     
