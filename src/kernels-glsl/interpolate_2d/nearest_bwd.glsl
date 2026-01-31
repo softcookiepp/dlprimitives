c34142e4df9c4aa48e9ae8aa73b7094b83a9175f
softcookiepp <softcookiepp@mailfence.com>
2026-01-31 09:23:40 -0800

    nearest_bwd.glsl compiles

--- src/kernels-glsl/interpolate_2d/nearest_bwd.glsl
+++ src/kernels-glsl/interpolate_2d/nearest_bwd.glsl
@@ -5,7 +5,10 @@
 #include "common_functions.glsl"
 
 
-
+#if USE_BDA == 0
+	layout(binding = 0, std430) buffer dx_buf { dtype dx[]; };
+	layout(binding = 1, std430) readonly buffer dy_buf { dtype dy[]; };
+#endif
 
 layout(push_constant, std430) uniform nearest_bwd
 {
@@ -40,28 +43,28 @@ void main()
 
     uint step_src = srcW*srcH;
     uint step_tgt = tgtW*tgtH;
-    dx += dx_offset + n0 * step_src;
-    dy += dy_offset + n0 * step_tgt;
+    uint dx_ = dx_offset + n0 * step_src;
+    uint dy_ = dy_offset + n0 * step_tgt;
 
     uint tgt_r0 = get_tgt_pos(src_r,  scale_y, tgtH, offset);
     uint tgt_r1 = get_tgt_pos(src_r+1,scale_y, tgtH, offset);
     uint tgt_c0 = get_tgt_pos(src_c,  scale_x, tgtW, offset);
     uint tgt_c1 = get_tgt_pos(src_c+1,scale_x, tgtW, offset);
    
-    dx += src_r * srcW + src_c;
+    dx_ += src_r * srcW + src_c;
 
     for(uint n=n0;n<n1;n++) {
         dtype grad = 0;
         for(uint r=tgt_r0;r<tgt_r1;r++) {
             for(uint c=tgt_c0;c<tgt_c1;c++) {
-                grad += dy[r*tgtW+c];
+                grad += dy[r*tgtW + c + dy_];
             }
         }
         if(beta == 0)
-            *dx = grad;
+            dx[dx_] = grad;
         else
-            *dx = beta * *dx + grad;
-        dx += step_src;
-        dy += step_tgt;
+            dx[dx_] = beta * dx[dx_] + grad;
+        dx_ += step_src;
+        dy_ += step_tgt;
     }
 }
