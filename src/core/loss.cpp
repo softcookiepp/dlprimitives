6e8ff000b24deaf0d052251a94a5cd3d8421be18
softcookiepp <softcookiepp@mailfence.com>
2026-04-20 11:31:45 -0700

    up we go

--- src/core/loss.cpp
+++ src/core/loss.cpp
@@ -232,7 +232,11 @@ namespace core {
         std::string itype;
         switch(lbl.dtype()) {
         case int32_data: itype = "int"; break;
+#if VULKAN_API
+        case int64_data: itype = "int64_t"; break;
+#else
         case int64_data: itype = "long"; break;
+#endif
         case float_data: itype = "float"; break;
         default: throw NotImplementedError("Unsupported type");
         }
