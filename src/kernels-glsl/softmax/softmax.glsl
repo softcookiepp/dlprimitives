aa99c9e595335f0c9321ecc45c9580f81994fc9a
softcookiepp <softcookiepp@mailfence.com>
2026-04-20 10:35:13 -0700

    up we go

--- src/kernels-glsl/softmax/softmax.glsl
+++ src/kernels-glsl/softmax/softmax.glsl
@@ -103,8 +103,7 @@ void main()
         }
     #endif
     my_work_group_reduce_add(sum, WG_SIZE);
-    // just for the sake of testing stuff
-    sum = 1.0;
+    barrier();
 
     #if LOG_SM == 0
 		val = dtype(1) / sum;
