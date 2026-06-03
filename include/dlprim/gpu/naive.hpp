b3feba32c53f8f26703c2bdac81dfce5ff0f5b23
softcookiepp <softcookiepp@mailfence.com>
2026-06-03 10:37:46 -0700

    added cpp file for naive implementations

---
+++ include/dlprim/gpu/naive.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#if VULKAN_API
+#include <dlprim/core/util.hpp>
+
+namespace dlprim
+{
+
+namespace gpu
+{
+
+Tensor convolution_naive();
+
+}
+
+} // dlprim
+
+#endif
---
+++ src/naive.cpp
@@ -0,0 +1,12 @@
+#if VULKAN_API // if no vulkan, no need for this crap
+
+#include <dlprim/gpu/naive.hpp>
+#include <dlprim/gpu/program_cache.hpp>
+#include <dlprim/ops/scal.hpp>
+#include <iostream>
+#include <clblast_vk.h>
+#include <dlprim/core/pointwise.hpp>
+
+
+
+#endif
