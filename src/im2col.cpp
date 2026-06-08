ce471e2d963c2431da914ca97751085fc7caa0d1
softcookiepp <softcookiepp@mailfence.com>
2026-06-08 09:19:38 -0700

    whyyyyyyyy

--- src/im2col.cpp
+++ src/im2col.cpp
@@ -55,7 +55,8 @@ void im2col(const ExecutionContext& e,
 	const uint32_t threads_per_block = 1024;
 	
 	uint32_t block_num = (num_kernels - 1) / threads_per_block + 1;
-	im2colKernel->run({block_num, 1, 1}, {});
+	//im2colKernel->run({block_num, 1, 1}, {});
+	im2colKernel->run({1024, 1, 1}, {});
 }
 
 template <typename T>
