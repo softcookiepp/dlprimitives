334054c14d979506ab913d671618b03818ee0298
softcookiepp <softcookiepp@mailfence.com>
2026-06-09 15:20:19 -0700

    launch code for col2im_batched builds

--- src/im2col.cpp
+++ src/im2col.cpp
@@ -201,6 +201,90 @@ void col2im(
 	col2im_kernel->run({block_num, 1, 1}, {});
 }
 
+void col2im_batched(
+		const ExecutionContext& e,
+		const tart::buffer_ptr& data_col,
+		const uint32_t data_col_offset,
+		const uint32_t col_batch_stride,
+		const uint32_t nbatch,
+		const uint32_t channels,
+		const uint32_t height,
+		const uint32_t width,
+		const uint32_t height_col,
+		const uint32_t width_col,
+		const uint32_t patch_height,
+		const uint32_t patch_width,
+		const uint32_t pad_height,
+		const uint32_t pad_width,
+		const uint32_t stride_height,
+		const uint32_t stride_width,
+		const uint32_t dilation_height,
+		const uint32_t dilation_width,
+		const tart::buffer_ptr& data_im,
+		const uint32_t data_im_offset,
+		const uint32_t im_batch_stride,
+		const DataType dtype)
+{
+	const uint32_t num_kernels = channels * height * width;
+	const uint32_t output_numel = nbatch * num_kernels;
+	if (output_numel == 0) {
+		return;	// No work to do
+	}
+	
+#if 1
+	// get the kernel
+	Context ctx(e);
+	tart::program_ptr prg = Cache::instance().get_program(ctx, "col2im_torch", "dtype", data_type_to_opencl_type(dtype));
+	tart::kernel_ptr col2im_kernel = prg->getKernel("col2im_batched");
+	
+	size_t p = 0;
+	col2im_kernel->setArg(p++, num_kernels);
+	col2im_kernel->setArg(p++, data_col);
+	col2im_kernel->setArg(p++, data_col_offset);
+	col2im_kernel->setArg(p++, col_batch_stride);
+	col2im_kernel->setArg(p++, nbatch);
+	col2im_kernel->setArg(p++, height);
+	col2im_kernel->setArg(p++, width);
+	col2im_kernel->setArg(p++, patch_height);
+	col2im_kernel->setArg(p++, patch_width);
+	col2im_kernel->setArg(p++, stride_height);
+	col2im_kernel->setArg(p++, stride_width);
+	col2im_kernel->setArg(p++, dilation_height);
+	col2im_kernel->setArg(p++, dilation_width);
+	col2im_kernel->setArg(p++, height_col);
+	col2im_kernel->setArg(p++, width_col);
+	col2im_kernel->setArg(p++, data_im);
+	col2im_kernel->setArg(p++, data_im_offset);
+	col2im_kernel->setArg(p++, im_batch_stride);
+	
+	uint32_t block_num = (num_kernels - 1) / 512 + 1;
+	col2im_kernel->run({block_num, 1, 1}, {});
+#else
+	// To avoid involving atomic operations, we will launch one kernel per
+	// bottom dimension, and then in the kernel add up the top dimensions.
+	// CUDA_NUM_THREADS = 1024
+	col2im_batched_kernel<<<GET_BLOCKS(output_numel, 512), 512, 0, stream>>>(
+		num_kernels,
+		data_col,
+		col_batch_stride,
+		nbatch,
+		height,
+		width,
+		patch_height,
+		patch_width,
+		pad_height,
+		pad_width,
+		stride_height,
+		stride_width,
+		dilation_height,
+		dilation_width,
+		height_col,
+		width_col,
+		data_im,
+		im_batch_stride);
+#endif
+}
+
 
 } // namespace gpu
 	
