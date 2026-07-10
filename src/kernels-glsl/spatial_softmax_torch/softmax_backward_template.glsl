cf22c8ba75c0e08124efe370d1d1e988ec792e2b
me <softcookiepp@protonmail.com>
2026-07-10 00:15:51 -0700

    gonna try translating this one more time

--- src/kernels-glsl/spatial_softmax_torch/softmax_backward_template.glsl
+++ src/kernels-glsl/spatial_softmax_torch/softmax_backward_template.glsl
@@ -37,44 +37,38 @@ layout(push_constant, std430) uniform cunn_SpatialSoftMaxBackward
 
 void do_softmax_backward()
 {
-	const uint outer_stride = inner_size * dim_size;
-	const uint dim_stride = inner_size;
+	extern __shared__ unsigned char smem[];
+	auto sdata = reinterpret_cast<accscalar_t*>(smem);
+	const uint32_t outer_stride = inner_size * dim_size;
+	const uint32_t dim_stride = inner_size;
 
-	for (uint outer_index = blockIdx.x; outer_index < outer_size; outer_index += gridDim.x)
-	{
-		const uint outer_offset = outer_index * outer_stride;
-		for (uint inner_index = blockIdx.y * blockDim.y + threadIdx.y; inner_index < inner_size; inner_index += blockDim.y * gridDim.y)
-		{
-			const uint data_offset = outer_offset + inner_index;
+	for (uint32_t outer_index = blockIdx.x; outer_index < outer_size; outer_index += gridDim.x) {
+		const uint32_t outer_offset = outer_index * outer_stride;
+		for (uint32_t inner_index = blockIdx.y * blockDim.y + threadIdx.y; inner_index < inner_size; inner_index += blockDim.y * gridDim.y) {
+			const uint32_t data_offset = outer_offset + inner_index;
 			// See the comment in forward kernel
-			if (blockDim.x > 1)
-			{
-				dtype sum = 0;
-				for (uint d = threadIdx.x; d < dim_size; d += blockDim.x)
-					sum += gradOutput[data_offset + d * dim_stride + gradOutputOffset];
-				//sum = spatialBlockReduceX<dtype, Add>(sdata, sum);
-				spatialBlockReduceX(sum, add_fn, sdata, 0, sum);
-				
-				Epilogue epilogue = Epilogue(sum);
-				for (uint d = threadIdx.x; d < dim_size; d += blockDim.x)
-				{
-					gradInput[data_offset + d * dim_stride + gradInputOffset] =
-						do_epilogue(epilogue, gradOutput[data_offset + d * dim_stride + gradOutputOffset], output_[data_offset + d * dim_stride + output_offset]);
-				}
-			}
-			else
-			{
-				dtype sum = 0;
-				//for (uint d = 0; d < dim_size; d++)
-				for (uint d = 0; d < 2; d++)
-					sum += gradOutput[data_offset + d * dim_stride + gradOutputOffset];
+			if (blockDim.x > 1) {
+				accscalar_t sum = 0;
+				for (uint32_t d = threadIdx.x; d < dim_size; d += blockDim.x)
+					sum += gradOutput[data_offset + d * dim_stride];
+				sum = spatialBlockReduceX<accscalar_t, Add>(sdata, sum);
 
-				Epilogue epilogue = Epilogue(sum);
-				for (uint d = 0; d < dim_size; d++)
-				{
-					gradInput[data_offset + d * dim_stride + gradInputOffset] =
-						do_epilogue(epilogue, gradOutput[data_offset + d * dim_stride + gradOutputOffset],
-										output_[data_offset + d * dim_stride + output_offset]);
+				Epilogue<scalar_t, accscalar_t, outscalar_t> epilogue(sum);
+				for (uint32_t d = threadIdx.x; d < dim_size; d += blockDim.x) {
+					gradInput[data_offset + d * dim_stride] =
+						epilogue(gradOutput[data_offset + d * dim_stride],
+										output[data_offset + d * dim_stride]);
+				}
+			} else {
+				accscalar_t sum = 0;
+				for (uint32_t d = 0; d < dim_size; d++)
+					sum += gradOutput[data_offset + d * dim_stride];
+
+				Epilogue<scalar_t, accscalar_t, outscalar_t> epilogue(sum);
+				for (uint32_t d = 0; d < dim_size; d++) {
+					gradInput[data_offset + d * dim_stride] =
+						epilogue(gradOutput[data_offset + d * dim_stride],
+										output[data_offset + d * dim_stride]);
 				}
 			}
 		}
