3afc366d085515f5c6ee80a00e3518522a2ea49c
softcookiepp <softcookiepp@mailfence.com>
2026-07-27 15:28:10 -0700

    started refactoring the activation code

--- src/kernels-glsl/common/defs.glsl
+++ src/kernels-glsl/common/defs.glsl
@@ -85,29 +85,54 @@
 #define ACTIVATION ACTIVATION_IDENTITY
 #endif
 
-#if ACTIVATION == ACTIVATION_IDENTITY
-#   define ACTIVATION_F(x) (x)
-#   define ACTIVATION_FINV(y,dy) (dy)
-#   define ACTIVATION_NAME identity
-#elif ACTIVATION == ACTIVATION_RELU
-#   define ACTIVATION_F(x) (max((x),dtype(0)))
-#   define ACTIVATION_FINV(y,dy)  ((y>0)?dy:0)
-#   define ACTIVATION_NAME relu
-#elif ACTIVATION == ACTIVATION_TANH
-#   define ACTIVATION_F(x) (tanh((x)))
-#   define ACTIVATION_FINV(y,dy) ((1-(y)*(y))*(dy))
-#   define ACTIVATION_NAME tanh 
-#elif ACTIVATION == ACTIVATION_SIGMOID
-#   define ACTIVATION_F(x) (dtype(1) / (dtype(1) + exp(-(x))))
-#   define ACTIVATION_FINV(y,dy) ((y)*(1-(y))*(dy))
-#   define ACTIVATION_NAME sigmoid
-#elif ACTIVATION == ACTIVATION_RELU6
-#   define ACTIVATION_F(x) (min(max((x),dtype(0)), dtype(6)))
-#   define ACTIVATION_FINV(y,dy)  ((0<y && y<6)?dy:0)
-#   define ACTIVATION_NAME relu6
+#if 1
+
+	#if ACTIVATION == ACTIVATION_IDENTITY
+	#   define ACTIVATION_F(x) (x)
+	#   define ACTIVATION_FINV(y,dy) (dy)
+	#   define ACTIVATION_NAME identity
+	#elif ACTIVATION == ACTIVATION_RELU
+	#   define ACTIVATION_F(x) (max((x),dtype(0)))
+	#   define ACTIVATION_FINV(y,dy)  ((y>0)?dy:0)
+	#   define ACTIVATION_NAME relu
+	#elif ACTIVATION == ACTIVATION_TANH
+	#   define ACTIVATION_F(x) (tanh((x)))
+	#   define ACTIVATION_FINV(y,dy) ((1-(y)*(y))*(dy))
+	#   define ACTIVATION_NAME tanh 
+	#elif ACTIVATION == ACTIVATION_SIGMOID
+	#   define ACTIVATION_F(x) (dtype(1) / (dtype(1) + exp(-(x))))
+	#   define ACTIVATION_FINV(y,dy) ((y)*(1-(y))*(dy))
+	#   define ACTIVATION_NAME sigmoid
+	#elif ACTIVATION == ACTIVATION_RELU6
+	#   define ACTIVATION_F(x) (min(max((x),dtype(0)), dtype(6)))
+	#   define ACTIVATION_FINV(y,dy)  ((0<y && y<6)?dy:0)
+	#   define ACTIVATION_NAME relu6
+	#else
+	#   error "Unknown activation"
+	#endif 
+
 #else
-#   error "Unknown activation"
-#endif 
+	// eventually all activations will be based on specialization constants in order to reduce source compilation overhead
+	dtype ACTIVATION_F(dtype x)
+	{
+		if (ACTIVATION == ACTIVATION_IDENTITY) return x;
+		else if (ACTIVATION == ACTIVATION_RELU) return max((x),dtype(0));
+		else if (ACTIVATION == ACTIVATION_TANH) return tanh(x);
+		else if (ACTIVATION == ACTIVATION_SIGMOID) return (dtype(1) / (dtype(1) + exp(-(x))));
+		else if (ACTIVATION == ACTIVATION_RELU6) return (min(max((x),dtype(0)), dtype(6)));
+		return dtype(0.0);
+	}
+
+	dtype ACTIVATION_FINV(dtype y, dtype dy)
+	{
+		if (ACTIVATION == ACTIVATION_IDENTITY) return dy;
+		else if (ACTIVATION == ACTIVATION_RELU) return ((y>0)?dy:0);
+		else if (ACTIVATION == ACTIVATION_TANH) return ((1-(y)*(y))*(dy));
+		else if (ACTIVATION == ACTIVATION_SIGMOID) return ((y)*(1-(y))*(dy));
+		else if (ACTIVATION == ACTIVATION_RELU6) return (min(max((x),dtype(0)), dtype(6)));
+		return dtype(0.0);
+	}
+#endif
 
 #ifndef USE_UNROLL
 	#define USE_UNROLL 0
