befe2b486430759b0a397aa2903408935b358075
softcookiepp <softcookiepp@mailfence.com>
2026-07-28 09:40:34 -0700

    preparing pooling kernels to use specialization constants

--- src/kernels-glsl/global_pooling/global_pooling_bwd.glsl
+++ src/kernels-glsl/global_pooling/global_pooling_bwd.glsl
@@ -42,9 +42,7 @@ shared dtype reduce_vals[WG_SIZE];
         
 void main()
 {
-    #if POOL_MODE == 0
     uint x_ = x_offset;
-    #endif
     uint dx_ = dx_offset;
     uint out_ = out_offset;
     
@@ -55,92 +53,89 @@ void main()
 
     uint c = get_global_id(1) * ITEMS_PER_WI;
 
-    #if POOL_MODE == 0
-    x_  += b * over;
-    #endif
+    if (POOL_MODE == 0)
+	{
+		x_  += b * over;
+    }
     dx_ += b * over;
     out_ += b;
 
+	if (POOL_MODE == 0)
+	{
+		dtype val = -DTYPE_MAX;
+		uint index = -1;
+		for(uint i=0;i<ITEMS_PER_WI;i++) {
+			if(c+i < over) {
+				dtype tmp = x[c+i + x_];
+				if(tmp > val) {
+					index = c+i;
+					val = tmp;
+				}
+			}
+		}
 
-    
-    // REDUCE_PREPARE(WG_SIZE,dtype);
+		// __local uint reduce_indx[WG_SIZE];
+		// __local dtype reduce_vals[WG_SIZE];
 
-#if POOL_MODE == 0
+		uint lid = my_get_local_wg_id();
+		reduce_indx[lid] = index;
+		reduce_vals[lid] = val;
+		barrier();
+		for(uint i=WG_SIZE / 2; i > 0 ; i>>=1) {
+			if(lid < i) {
+				dtype val;
+				uint ind;
+				dtype vl = reduce_vals[lid];
+				uint   il = reduce_indx[lid];
+				dtype vr = reduce_vals[lid+i];
+				uint   ir = reduce_indx[lid+i];
+				if(vl > vr) {
+					ind=il;
+					val=vl;
+				}
+				else if(vr > vl) {
+					ind=ir;
+					val=vr;
+				}
+				else { // vr == vl
+					if(il < ir) {
+						ind=il;
+						val=vl;
+					}
+					else {
+						ind=ir;
+						val=vr;
+					}
+				}
+				reduce_vals[lid] = val;
+				reduce_indx[lid] = ind;
+			}
+			barrier(); 
+		}
 
-    dtype val = -DTYPE_MAX;
-    uint index = -1;
-    for(uint i=0;i<ITEMS_PER_WI;i++) {
-        if(c+i < over) {
-            dtype tmp = x[c+i + x_];
-            if(tmp > val) {
-                index = c+i;
-                val = tmp;
-            }
-        }
-    }
-
-    // __local uint reduce_indx[WG_SIZE];
-    // __local dtype reduce_vals[WG_SIZE];
-
-    uint lid = my_get_local_wg_id();
-    reduce_indx[lid] = index;
-    reduce_vals[lid] = val;
-    barrier();
-    for(uint i=WG_SIZE / 2; i > 0 ; i>>=1) {
-        if(lid < i) {
-            dtype val;
-            uint ind;
-            dtype vl = reduce_vals[lid];
-            uint   il = reduce_indx[lid];
-            dtype vr = reduce_vals[lid+i];
-            uint   ir = reduce_indx[lid+i];
-            if(vl > vr) {
-                ind=il;
-                val=vl;
-            }
-            else if(vr > vl) {
-                ind=ir;
-                val=vr;
-            }
-            else { // vr == vl
-                if(il < ir) {
-                    ind=il;
-                    val=vl;
-                }
-                else {
-                    ind=ir;
-                    val=vr;
-                }
-            }
-            reduce_vals[lid] = val;
-            reduce_indx[lid] = ind;
-        }
-        barrier(); 
-    }
-
-    uint target_index = reduce_indx[0];
-    dtype store = outp[out_];
-    for(uint i=0;i<ITEMS_PER_WI;i++) {
-        if(c+i < over) {
-            dtype val = (c + i == target_index) ? store : 0;
-            if(factor == 0)
-                dx[c+i + dx_] = val;
-            else
-                dx[c+i + dx_] = dx[c+i + dx_] * factor + val;
-        }
-    }
-#else
-    dtype store = outp[out_] * scale;
-    
-    for(uint i=0;i<ITEMS_PER_WI;i++) {
-        if(c+i < over) {
-            if(factor == 0)
-                dx[c+i + dx_] = store;
-            else
-                dx[c+i + dx_] = dx[c+i + dx_] * factor + store;
-        }
-    }
-    
-#endif
-
+		uint target_index = reduce_indx[0];
+		dtype store = outp[out_];
+		for(uint i=0;i<ITEMS_PER_WI;i++) {
+			if(c+i < over) {
+				dtype val = (c + i == target_index) ? store : 0;
+				if(factor == 0)
+					dx[c+i + dx_] = val;
+				else
+					dx[c+i + dx_] = dx[c+i + dx_] * factor + val;
+			}
+		}
+	}
+	else
+	{
+		dtype store = outp[out_] * scale;
+		
+		for(uint i=0;i<ITEMS_PER_WI;i++) {
+			if(c+i < over) {
+				if(factor == 0)
+					dx[c+i + dx_] = store;
+				else
+					dx[c+i + dx_] = dx[c+i + dx_] * factor + store;
+			}
+		}
+	}
 }
