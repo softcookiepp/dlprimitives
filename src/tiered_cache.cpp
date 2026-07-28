792f00bcb214131af0a289b4386cd15863d7e4cf
softcookiepp <softcookiepp@mailfence.com>
2026-07-28 14:06:14 -0700

    wait what

--- src/tiered_cache.cpp
+++ src/tiered_cache.cpp
@@ -48,7 +48,7 @@ AllPrograms::AllPrograms(const tart::device_ptr& device, const std::vector<DataT
 	{
 		// any programs that don't require a dtype
 		mPoolingProgram = gpu::Cache::instance().get_program(ctx, "pooling");
-		mGlobalPoolingProgram = gpu::Cache::instance().get_program(ctx, "global_pooling");
+		//mGlobalPoolingProgram = gpu::Cache::instance().get_program(ctx, "global_pooling");
 	}
 	else if (dtypes.size() == 1)
 	{
