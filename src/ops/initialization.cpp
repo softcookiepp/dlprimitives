ec658a6d9a0a69e98b373682bef2a727549302da
softcookiepp <softcookiepp@mailfence.com>
2026-08-12 09:58:44 -0700

    up

--- src/ops/initialization.cpp
+++ src/ops/initialization.cpp
@@ -20,82 +20,12 @@ void set_to_zero(Tensor &t,ExecutionContext const &e)
     }
 }
 
-namespace {
-    template<typename T>
-    void fill_value(Tensor &t,T v)
-    {
-        T *p= t.data<T>();
-        size_t size = t.shape().total_size();
-        for(size_t i=0;i<size;i++)
-            p[i] = v;
-    }
-}
-
 void set_to_constant(Tensor &t,double value,ExecutionContext const &e)
 {
     {
         core::fill_tensor(t,value);
     }
 }
-
-class UrandomConverter {
-public:
-    UrandomConverter(float min,float max)
-    {
-        scale_ = (max-min);
-        offset_ = min;
-    }
-    template<typename T>
-    void convert(T &vec)
-    {
-        for(auto &val : vec) {
-            val = val * scale_ + offset_;
-        }
-    }
-    float scale_,offset_;
-};
-
-class BernoulliConverter {
-public:
-    BernoulliConverter(float p) : p_(p)
-    {
-    }
-    template<typename T>
-    void convert(T &vec)
-    {
-        for(auto &val : vec) {
-            val = val < p_ ? 1 : 0;
-        }
-    }
-    float p_;
-};
-
-
-
-
-class NormalConverter {
-public:
-    NormalConverter(float mu,float sigma)
-    {
-        mu_ = mu;
-        sigma_ = sigma;
-    }
-    void convert(philox::float_result_type &f)
-    {
-        convert_pair(f[0],f[1]);
-        convert_pair(f[2],f[3]);
-    }
-private:
-    void convert_pair(float &r1,float &r2)
-    {
-        float scale = std::sqrt(-2.0f*std::log(1.0f - r1)) * sigma_;
-        ///                                     r1 in [0, 1)
-        float angle = (2.0f*3.1415926535f)*r2;
-        r1 = scale*std::cos(angle) + mu_;
-        r2 = scale*std::sin(angle) + mu_;
-    }
-    float mu_,sigma_;
-};
 
 void get_seed_seq(size_t total,RandomState &state,RandomState::seed_type &seed,RandomState::sequence_type &seq)
 {
