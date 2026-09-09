f7cae014e6f00bb8da756b9daa27731050a961de
softcookiepp <softcookiepp@mailfence.com>
2026-09-09 08:13:45 -0700

    purposefully set ypos to 0

--- src/kernels-glsl/pointwise-reduce-naive/pointwise-reduce-naive-template.glsl
+++ src/kernels-glsl/pointwise-reduce-naive/pointwise-reduce-naive-template.glsl
@@ -103,6 +103,8 @@ void pointwise_reduce_naive_impl()
 	// determine position, exit if out of bounds
 	// In this kernel, yShape is the one
 	Shape yPos = getPosFromTriIndex(yShape, DIMS);
+	for (size_t i = 0; i < DIMS; i += 1)
+		yPos.s[i] = 0;
 	if (!posValid(yShape, yPos, DIMS)) return;
 	
 	// Need to iterate over all possible elements in the reduction shape.
