53655b46e110134647cc12a4fe446477a5d77da1
me <softcookiepp@protonmail.com>
2026-09-09 19:24:06 -0700

    welp

--- src/core/pointwise.cpp
+++ src/core/pointwise.cpp
@@ -448,8 +448,7 @@ namespace core {
 				static_cast<uint32_t>(xShape.size()),
 				static_cast<uint32_t>(reduceDimShape.size()),
 				numReduceElems,
-				workPerThread,
-				wgxSize
+				workPerThread
 			};
 			k->enqueue(global, spec);
 		}
