efb1e830134a61abccc3f91d22c83fca1694b1c0
me <softcookiepp@protonmail.com>
2026-09-09 21:11:38 -0700

    a failsafe?

--- src/core/pointwise.cpp
+++ src/core/pointwise.cpp
@@ -433,7 +433,7 @@ namespace core {
 			k->setArg(p++, yInitValues);
 			k->setArg(p++, ws);
 			
-			uint32_t workPerThread = 2;
+			uint32_t workPerThread = 3;
 			uint32_t r = numReduceElems % workPerThread;
 			uint32_t wgxSize = numReduceElems / workPerThread;
 			if (r > 0 || wgxSize == 0) wgxSize += 1;
