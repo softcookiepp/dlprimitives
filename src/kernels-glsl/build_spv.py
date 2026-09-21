e652ee2b2d86743cf31593f0c1d3b768e7242f9e
softcookiepp <softcookiepp@mailfence.com>
2026-09-21 16:32:41 -0700

    finally starting to implement compile-time SPIR-V generation

---
+++ src/kernels-glsl/build_spv.py
@@ -0,0 +1,25 @@
+import itertools
+
+DTYPE_ENUM = [
+	0,
+	1,
+	2,
+	#3, BF16, not implemented
+	4,
+	5,
+	#6, MXFP4, not implemented
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14
+	#15, FP8, not implemented
+	#16 same
+]
+
+if __name__ == "__main__":
+	pass
+	
